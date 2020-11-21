9ba597bb42c18a20ad6a94a04e16ae1d991fb09a
Alexander Rabmer <alexander.rabmer@gmx.at>
2020-11-21 15:06:47 +0100

    Added Fahrradhersteller spring project

---
+++ Code/Fahrradhersteller/src/main/java/Fahrradhersteller/FahrradherstellerApplication.java
@@ -0,0 +1,13 @@
+package Fahrradhersteller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FahrradherstellerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FahrradherstellerApplication.class, args);
+	}
+
+}
---
+++ Code/Fahrradhersteller/src/test/java/Fahrradhersteller/FahrradherstellerApplicationTests.java
@@ -0,0 +1,13 @@
+package Fahrradhersteller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FahrradherstellerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
