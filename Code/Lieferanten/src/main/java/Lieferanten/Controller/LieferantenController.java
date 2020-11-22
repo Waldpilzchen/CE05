3ce194c2ea0c2c5e132316a8a8d8e8384ce0de66
Alexander Rabmer <alexander.rabmer@gmx.at>
2020-11-22 20:11:09 +0100

    Added Lieferanten Project

---
+++ Code/Lieferanten/src/main/java/Lieferanten/Controller/LieferantenController.java
@@ -0,0 +1,8 @@
+package Lieferanten.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LieferantenController {
+    
+}
---
+++ Code/Lieferanten/src/main/java/Lieferanten/LieferantenApplication.java
@@ -0,0 +1,13 @@
+package Lieferanten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LieferantenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LieferantenApplication.class, args);
+	}
+
+}
